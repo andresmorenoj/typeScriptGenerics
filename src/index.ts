e461b4b6534a16403dcd626e8328b5ed3d0dec4b
Andres Moreno <andresmorenojf@gmail.com>
2024-01-20 00:07:52 -0500

    Built-In Generic

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// const nums: number[] = []
+const nums: Array<number> = []
+const colors: Array<string> = []
+
+const inputEl = document.querySelector<HTMLInputElement>("#username");
+const btn = document.querySelector<HTMLButtonElement>(".login")
